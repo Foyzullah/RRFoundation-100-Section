4b775c1ed00c69aa49d88efa3430da1c784fd1b3
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2017-12-19 13:04:02 +0600

    Parallax Animation

---
+++ Class-31/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        var s=skrollr.init({
+            vendor:function(data){
+                
+            }
+        })
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
