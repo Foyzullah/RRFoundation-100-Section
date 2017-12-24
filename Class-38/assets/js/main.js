9e8e511669abda34801efd3af14f0317309ec56c
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2017-12-25 00:06:20 +0600

    Overlay Hero Section

---
+++ Class-38/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".homepage-slides").owlCarousel({
+           items: 1,
+           nav: true,
+           dots:true,
+           autoplay:false,
+           navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+       });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
