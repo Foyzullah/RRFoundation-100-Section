daa64023b40c59f5a06f8ae69a3149e45244d73e
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-06 23:52:41 +0600

    Special Carousel

---
+++ Class-46/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".featured-trailer-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay:false
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
