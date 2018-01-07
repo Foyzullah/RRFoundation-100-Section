888dfdec92cb29bd2dc17cd88938af0e7d35efa8
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-07 17:12:19 +0600

    Testimonial Carousel

---
+++ Class-47/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            dots: true,
+            nav: false,
+            autoplay: true,
+            loop: true
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
