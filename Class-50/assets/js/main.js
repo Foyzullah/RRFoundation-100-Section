4dd94ea75b1b951c3f7715cd0cabd44c49523f1d
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-08 23:26:03 +0600

    Owl-carousel customize

---
+++ Class-50/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".listing-carousel").owlCarousel({
+            items: 3,
+            loop: true,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            margin: 30
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
