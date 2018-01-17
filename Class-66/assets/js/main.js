aa276f1e81002bc1464f1c0185692aee215b31b2
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-17 18:43:11 +0600

    Sticky Area

---
+++ Class-66/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+      $(".header-area").sticky();
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
