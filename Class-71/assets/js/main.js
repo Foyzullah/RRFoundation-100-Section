6a9360e85b7598fa65f7d455d792b9bee35fbc0b
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-21 17:03:23 +0600

    Bootstrap Tooltip & Popover

---
+++ Class-71/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
