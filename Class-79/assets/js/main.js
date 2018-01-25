1b5aa6fb6be1b243951faeb849759a88faa0e71b
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-25 23:25:46 +0600

    Mailchimp & Ajax Mailchimp

---
+++ Class-79/assets/js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+        $(".earlyaccess").ajaxChimp({
+            url:'http://nothing.us17.list-manage.com/subscribe/post?u=32e24eab6cacc93b5251a79b6&amp;id=6ca1c3f4d8'
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
+
+
+
