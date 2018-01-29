abaad1600f4e5d77579211160411bcfb711db03a
Foyzullah <33192958+Foyzullah@users.noreply.github.com>
2018-01-29 18:56:30 +0600

    Creative Testimonial Carousel

---
+++ Class-85/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            dots: false,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+            autoplay: false,
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
